e161803377abc8f4555b492953df9306ea0b373d
Scott Petersen <scott@nutech.com>
2014-12-12 13:59:40 -0800

    Initial import of template.

---
+++ src/Main.hx
@@ -0,0 +1,36 @@
+import luxe.Input;
+
+class Main extends luxe.Game
+{
+    public override function ready() : Void
+    {
+        init_graphics();
+        init_input();
+    }
+
+    public override function update(dt : Float) : Void
+    {
+        handle_input();
+    }
+
+    public override function onkeyup(e : KeyEvent) : Void
+    {
+        if (e.keycode == Key.escape)
+            Luxe.shutdown();
+    }
+
+    private function handle_input() : Void
+    {
+
+    }
+
+    private function init_graphics() : Void
+    {
+
+    }
+
+    private function init_input() : Void
+    {
+
+    }
+}
